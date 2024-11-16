fc9fb1654d115a9c630514d4df89cfd4e46414a0
jagadeeswar-N-G <jagadeeswar8055@gmail.com>
2024-11-17 01:38:18 +0530

    this is the basic set up for the aws and generating the presigned URLs

--- apps/api/src/tweet/queries.ts
+++ apps/api/src/tweet/queries.ts
@@ -1,3 +1,4 @@
 export const queries = `#graphql
         getAllTweets: [Tweet]
+        getSignedURLForTweet(ImageType:String!):String
 `;
--- apps/api/src/tweet/resolvers.ts
+++ apps/api/src/tweet/resolvers.ts
@@ -1,14 +1,41 @@
 import { GraphqlContext } from "../types/interfaces";
 import { prisma } from "../db";
 import { Tweet } from "@prisma/client";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 type CreateTweetPayload = {
   content: string;
   imageURL?: string;
 };
 
+const s3 = new S3Client({
+  region: process.env.AWS_BUCKET_REGION!,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
+
 const queries = {
-  getAllTweets: () => prisma.tweet.findMany({orderBy:{ createdAt:"desc"}})
+  getAllTweets: () => prisma.tweet.findMany({orderBy:{ createdAt:"desc"}}),
+  getSignedURLForTweet: async(parent:any, {ImageType}:{ImageType:string},  context: GraphqlContext) =>{
+    if(!context.user) {
+      throw new Error("User not authenticated");
+    }
+    const allowedImageTypes = ["png", "jpg", "jpeg", "webp"];
+    if(!allowedImageTypes.includes(ImageType)) {
+      throw new Error("Invalid Image Type");
+    }
+    const putObjectCommand = new PutObjectCommand({
+      Bucket: process.env.AWS_BUCKET_NAME!,
+      Key: `${context.user.id}/${context.user.id}-${Date.now()}.${ImageType}`,
+    });
+
+    const SignedURL = await getSignedUrl(s3, putObjectCommand);
+    return SignedURL;
+   
+  }
 }
 const mutations = {
   createTweet: async (
